d22d63effe664bc4a7dda0a9483fd936cc6d8c79
Yusuf Badurohman <yusuf.badurohman@gmail.com>
2018-11-27 17:15:40 +0700

    add category & comment resource

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api
 from resources.Hello import Hello
+from resources.Category import CategoryResource
+from resources.Comment import CommentResource
 
 
 api_bp = Blueprint('api', __name__)
@@ -8,3 +10,5 @@ api = Api(api_bp)
 
 # Route
 api.add_resource(Hello, '/hello')
+api.add_resource(CategoryResource, '/category')
+api.add_resource(CommentResource, '/comment')
